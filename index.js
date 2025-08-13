507ac80d89d9910ffbc1184d9930204453256b11
xstructures <developer@xstructures.net>
2025-08-13 17:57:03 +0800

    Change Listener to all

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.use(express.json());
 const userRoutes = require('./routes/userRoutes');
 app.use('/users', userRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
 
